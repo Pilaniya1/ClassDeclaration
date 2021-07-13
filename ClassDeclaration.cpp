ba053c40aec0c96d4c9a94872c3a340a7cbae7c6
Vinay Pilaniya <82079006+Pilaniya1@users.noreply.github.com>
2021-07-13 20:16:24 +0530

    Add files via upload
    //How to declare a class?
    //It is a general form of class declarations is 
    //Class end's with Semicolon(;).
    
    //    class class_name
      //  {
        //    private:
          //  variable declarations;
            //function declarations;
            //public:
            //variable declarations;
            //function declarations;
        //};
    //}

---
+++ ClassDeclaration.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+
+    class item
+    {
+        int number; //Variable declarations
+        float cost;  //Private by Default
+    
+        public:
+        void getdata(int a,float b);  //Functions declaration
+        void putdata(void);  //using prototype.
+    };
+}
